35f565707c7476da61a43629261c3559d9af4d98
Corey Miller <coreyMiller353@gmail.com>
2019-12-20 04:30:22 -0500

    styled pages

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,21 +7,23 @@ class NavBar extends Component {
 		return (
 			<div className='NavBar'>
 				<h1>Flikr Search App</h1>
-				<li>
-					<NavLink className='NavBar-Link' exact to='/forest'>
-						Pictures of Trees
-					</NavLink>
-				</li>
-				<li>
-					<NavLink className='NavBar-Link' exact to='/city'>
-						Pictures of Cities
-					</NavLink>
-				</li>
-				<li>
-					<NavLink className='NavBar-Link' exact to='/space'>
-						Pictures of Space
-					</NavLink>
-				</li>
+				<ul>
+					<li>
+						<NavLink className='NavBar-Link' exact to='/forest'>
+							Pictures of Trees
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='NavBar-Link' exact to='/city'>
+							Pictures of Cities
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className='NavBar-Link' exact to='/space'>
+							Pictures of Space
+						</NavLink>
+					</li>
+				</ul>
 			</div>
 		);
 	}
